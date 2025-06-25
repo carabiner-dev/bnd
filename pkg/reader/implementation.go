e7693bd789831eff265f56071edf2ae50aaae682
Adolfo Garcia Veytia (puerco) <adolfo.garcia@uservers.net>
2025-06-25 00:09:53 -0600

    Add read subcommand and reader pkg
    Signed-off-by: Adolfo Garcia Veytia (puerco) <adolfo.garcia@uservers.net>

---
+++ pkg/reader/implementation.go
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package reader
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/carabiner-dev/ampel/pkg/attestation"
+)
+
+type clientImplementation interface {
+	Fetch(string) ([]attestation.Envelope, error)
+}
+
+type defaultClientImplementation struct{}
+
+func (impl *defaultClientImplementation) Fetch(uri string) ([]attestation.Envelope, error) {
+	agent, err := buildAgent()
+	if err != nil {
+		return nil, fmt.Errorf("creating new collector agent: %w", err)
+	}
+	if err := agent.AddRepositoryFromString(uri); err != nil {
+		return nil, fmt.Errorf("adding new repository: %w", err)
+	}
+
+	return agent.Fetch(context.Background())
+}
